a0b0a3b28ad3b2360a7255229cdab8b0f9d32b98
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-11-24 00:19:58 -0300

    Updates
    Updates

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let switchCtn = document.querySelector('#switch-cnt');
+let switchC1 = document.querySelector('#switch-c1');
+let switchC2 = document.querySelector('#switch-c2');
+let switchCircle = document.querySelector('.switch_circle');
+let switchBtn = document.querySelector('.switch_btn');
+let aContainer = document.querySelector('');
+let bContainer = document.querySelector('');
+let allButtons= document.querySelector('');
